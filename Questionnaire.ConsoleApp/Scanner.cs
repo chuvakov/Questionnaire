954e8bd1433065c59f1117ae96d50b0a4384e797
Александр Чуваков <4ymail@mail.ru>
2022-03-07 15:35:15 +0300

    + Печатанье вопроса + Получение ответа на вопрос + Запуск опроса + Считывание данных пользователя при прохождении опроса

---
+++ Questionnaire.ConsoleApp/Scanner.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Questionnaire.Core.Enums;
+using Questionnaire.Core.Models;
+
+namespace Questionnaire.ConsoleApp;
+
+public static class Scanner
+{
+    public static Person GetPerson()
+    {
+        string name = Scan("Введите имя: ");
+        int age = int.Parse(Scan("Введите возраст: ", 
+            x => int.TryParse(x, out int result)));
+        string sex = Scan("Введите пол [м/ж]: ", 
+            x => x.ToLower() == "м" || x.ToLower() == "ж");
+
+        return new Person()
+        {
+            Name = name,
+            Age = age,
+            Sex = sex == "м" ? Sex.Male : Sex.Female
+        };
+    }
+
+    private static string Scan(string title, Predicate<string> isValid = null)
+    {
+        lableStart:
+        Console.Write(title);
+
+        try
+        {
+            string data = Console.ReadLine();
+
+            if (isValid == null || isValid(data))
+            {
+                return data;
+            }
+
+            Console.WriteLine("Данные введены некоректно, попробуйте снова.");
+            goto lableStart;
+        }
+        catch
+        {
+            Console.WriteLine("Данные введены некоректно, попробуйте снова.");
+            goto lableStart;
+        }
+    }
+} // свичкейс
+ // делигаты
